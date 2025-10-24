c1fa225996770d18fed16f1e7b8a6a035f4ff43c
Gururaj A Sajjan <gururajsajjan03@gmail.com>
2025-10-24 15:05:13 +0530

    Add files via upload

---
+++ replace.py
@@ -0,0 +1,9 @@
+def replace_word(s):
+   print(s)
+   old=input("Enter the word to be replaced")
+   new=input("Enter the new word")
+   print(s.replace(old,new)) 
+    
+    
+s=input("Enter a string")
+replace_word(s)
---
+++ swap_case.py
@@ -0,0 +1,7 @@
+def swap(s):
+   text=s
+   return text.swapcase()
+
+s=input("Enter a string : ")
+
+print(swap(s))
